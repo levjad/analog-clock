07355a082417842a86ba84fcac543e4827c23ebf
Lev Jad <levjad@gmail.com>
2025-02-03 17:06:16 +0100

    Modernize code

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
-var canvas = document.getElementById('clock');
-var ctx = canvas.getContext('2d');
-var radius = canvas.height / 2;
+const canvas = document.getElementById('clock');
+const ctx = canvas.getContext('2d');
+let radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.9;
 
@@ -13,23 +13,22 @@ function drawClock() {
 function drawFace() {
 	ctx.beginPath();
 	ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-	ctx.fillStyle = 'white';
+	ctx.fillStyle = document.documentElement.classList.contains('dark') ? 'black' : 'white';
 	ctx.fill();
 
 	ctx.beginPath();
 	ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-	ctx.fillStyle = 'black';
+	ctx.fillStyle = document.documentElement.classList.contains('dark') ? 'white' : 'black';
 	ctx.fill();
 }
 
 function drawNumbers() {
-	var ang;
-	var num;
-	ctx.font = radius * 0.15 + 'px arial';
+	ctx.font = radius * 0.15 + 'px Arial';
 	ctx.textBaseline = 'middle';
 	ctx.textAlign = 'center';
-	for (num = 1; num < 13; num++) {
-		ang = num * Math.PI / 6;
+	ctx.fillStyle = document.documentElement.classList.contains('dark') ? 'white' : 'black';
+	for (let num = 1; num < 13; num++) {
+		const ang = num * Math.PI / 6;
 		ctx.rotate(ang);
 		ctx.translate(0, -radius * 0.85);
 		ctx.rotate(-ang);
@@ -41,18 +40,19 @@ function drawNumbers() {
 }
 
 function updateTime() {
-	var time = new Date();
-	var h = time.getHours();
-	var m = time.getMinutes();
-	var s = time.getSeconds();
-	h = h % 12;
-	h = h * Math.PI / 6 + m * Math.PI / (6 * 60) + s * Math.PI / (360 * 60);
-	drawPointer(h, radius * 0.5, radius * 0.07, 'black');
-	m = m * Math.PI / 30 + s * Math.PI / (30 * 60);
-	drawPointer(m, radius * 0.8, radius * 0.07, 'black');
-	s = s * Math.PI / 30;
-	drawPointer(s, radius * 0.9, radius * 0.02, 'red');
-	console.log('Stunde: ' + h + ' Minuten ' + m + ' Sekunden ' + s);
+	const time = new Date();
+	const h = time.getHours();
+	const m = time.getMinutes();
+	const s = time.getSeconds();
+
+	const hour = h % 12;
+	const hourAngle = hour * Math.PI / 6 + m * Math.PI / (6 * 60) + s * Math.PI / (360 * 60);
+	const minuteAngle = m * Math.PI / 30 + s * Math.PI / (30 * 60);
+	const secondAngle = s * Math.PI / 30;
+
+	drawPointer(hourAngle, radius * 0.5, radius * 0.07, document.documentElement.classList.contains('dark') ? 'white' : 'black');
+	drawPointer(minuteAngle, radius * 0.8, radius * 0.07, document.documentElement.classList.contains('dark') ? 'white' : 'black');
+	drawPointer(secondAngle, radius * 0.9, radius * 0.02, 'red');
 }
 
 function drawPointer(pos, length, width, color) {
@@ -67,4 +67,10 @@ function drawPointer(pos, length, width, color) {
 	ctx.rotate(-pos);
 }
 
-setInterval(drawClock, 1000);
+function animate() {
+	ctx.clearRect(-radius, -radius, canvas.width, canvas.height);
+	drawClock();
+	requestAnimationFrame(animate);
+}
+
+animate();
